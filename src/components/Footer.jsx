d9bcde478f176ee92648c102a40aaae80624ce9b
Steve-Don <92700131+Steve-Don@users.noreply.github.com>
2022-08-27 18:06:10 +0930

    keeper react app v1.0 setting up the whole frame

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const year = new Date();
+
+var currentYear = year.getFullYear();
+
+function Footer(){
+    return (
+        <footer>
+            <p>{currentYear +' '}copyright</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+//<h1> for a title and a <p> for the content.
+
+import React from "react";
+
+function Note(){
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>content</p>
+        </div>
+    )
+}
+
+export default Note;
