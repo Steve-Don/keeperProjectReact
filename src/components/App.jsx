0a03b22f72af2cb50aafa8de2f826895d5d40a9c
Steve-Don <92700131+Steve-Don@users.noreply.github.com>
2022-08-29 20:53:07 +0930

    Kepper react app v1.3 shorten the code by using the => arrow to passing the function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,19 @@ import Footer from "./Footer"
 import Note from "./Note"
 import notes from "./notes";
 
-function createNotes(notes){
-    return (
-    <Note
-        key = {notes.key}
-        title = {notes.title}
-        content = {notes.content}
-    />
-    )
-}
+
 
 function App(){
     return(
         <div>
             <Header />
             <Footer />
-            {notes.map(createNotes)}
+            {notes.map((notes)=>{<Note
+            key = {notes.key}
+            title = {notes.title}
+            content = {notes.content}
+    />   
+})}
         </div>
     )
 }
